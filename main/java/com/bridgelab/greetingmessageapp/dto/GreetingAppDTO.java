42daa4de38c44d0cc8251b07bb87f40acad8d53a
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-08-08 10:20:55 +0530

    REFACTOR : Using greeting model save the repositroy

--- main/java/com/bridgelab/greetingmessageapp/dto/GreetingAppDTO.java
+++ main/java/com/bridgelab/greetingmessageapp/dto/GreetingAppDTO.java
@@ -1,45 +1,14 @@
 package com.bridgelab.greetingmessageapp.dto;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
+@Data
 public class GreetingAppDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String message;
 
-    public GreetingAppDTO(String message) {
-        this.message = message;
-    }
-
     public GreetingAppDTO(long id, String message) {
         this.id = id;
-        this.message = message;
-    }
-
-    public GreetingAppDTO() {
-
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
         this.message = message;
     }
 }
--- main/java/com/bridgelab/greetingmessageapp/dto/GreetingUserDTO.java
+++ main/java/com/bridgelab/greetingmessageapp/dto/GreetingUserDTO.java
@@ -1,32 +1,11 @@
 package com.bridgelab.greetingmessageapp.dto;
 
+import lombok.Data;
 
+@Data
 public class GreetingUserDTO {
     String firstName;
     String lastName;
 
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-
-    }
-
-    @Override
-    public String toString() {
-        return "GreetingUserDTO{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
 }
